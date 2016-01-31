46e2f4ab1f07a6d184e7716abdbf614d3c890aac
Andrey Astashov <mvc.aaa@gmail.com>
2016-02-01 01:11:28 +0300

    Testing/configs

--- app/tests/codeception/config/config.php
+++ app/tests/codeception/config/config.php
@@ -3,24 +3,22 @@
  * Application configuration shared by all test types
  */
 return [
-    'language' => 'en-US',
-    'controllerMap' => [
-        'fixture' => [
-            'class' => 'yii\faker\FixtureController',
-            'fixtureDataPath' => '@tests/codeception/fixtures',
-            'templatePath' => '@tests/codeception/templates',
-            'namespace' => 'tests\codeception\fixtures',
-        ],
-    ],
-    'components' => [
-        'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
-        ],
-        'mailer' => [
-            'useFileTransport' => true,
-        ],
-        'urlManager' => [
-            'showScriptName' => true,
-        ],
-    ],
+	'language' => 'en-US',
+	'controllerMap' => [
+		'fixture' => [
+			'class' => 'yii\faker\FixtureController',
+			'fixtureDataPath' => '@tests/codeception/fixtures',
+			'templatePath' => '@tests/codeception/templates',
+			'namespace' => 'tests\codeception\fixtures',
+		],
+	],
+	'components' => [
+		'db' => require(__DIR__ . '/db.php'),
+		'mailer' => [
+			'useFileTransport' => true,
+		],
+		'urlManager' => [
+			'showScriptName' => true,
+		],
+	],
 ];
